0cdaf00c98ede19536292a9356bf9ddf082578f8
Gigabyte <amriliss@yandex.ru>
2023-05-11 23:31:45 +0300

    Code style 5

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -23,6 +23,8 @@ public class FilmController {
         id = 0;
         films = new HashMap<>();
     }
+
+
     @ResponseBody
     @GetMapping(value = "/films")
     public List<Film> getFilms() {
@@ -40,6 +42,8 @@ public class FilmController {
         films.put(film.getId(), film);
         return film;
     }
+
+
     @ResponseBody
     @PutMapping(value = "/films")
     public Film update(@Valid @RequestBody Film film) {
