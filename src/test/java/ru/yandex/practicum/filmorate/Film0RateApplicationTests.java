b84b6efe443cc70e50490a238559ef5fd8dba4cc
Gigabyte <amriliss@yandex.ru>
2023-06-12 11:45:45 +0300

    fix: code style6

--- src/test/java/ru/yandex/practicum/filmorate/Film0RateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/Film0RateApplicationTests.java
@@ -5,8 +5,6 @@ import org.assertj.core.api.AssertionsForClassTypes;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.context.SpringBootTest;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.model.Mpa;
@@ -26,7 +24,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 
 
-@AutoConfigureTestDatabase
+
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 class FilmoRateApplicationTests {
     private final UserDBStorage userStorage;
